07377844933b48bcffdb99e57f50d1098b9dba3a
n0o0b090lv <ernestspursw@gmail.com>
2025-10-30 15:18:48 +0200

    Spider fall

--- scripts/spiders_skeleton.gd
+++ scripts/spiders_skeleton.gd
@@ -2,5 +2,5 @@ extends Skeleton3D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _process(delta: float) -> void:
 	physical_bones_start_simulation()
