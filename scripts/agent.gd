23b04fe6c669d8c7b603935c2b01af3c23c11e99
n0o0b090lv <ernestspursw@gmail.com>
2025-11-02 22:57:51 +0200

    neuron class

--- scripts/agent.gd
+++ scripts/agent.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+
 @export var goal: Node3D
 @export var text: Label3D
 @export var line: MeshInstance3D
@@ -45,3 +46,37 @@ func updateVisualisation():
 	line.scale = Vector3(1, 1, distance * 20)
 	line.global_position = (goal.global_position + spider_skel.global_position) / 2
 	line.look_at(spider_skel.global_position, Vector3.UP)
+
+class Neuron_Layer:
+	var neurons = []
+	var output_size
+	func _init(p_inputs, p_neurons, p_outputs) -> void:
+		output_size = p_outputs
+		for i in p_neurons:
+			neurons.append(Neuron.new(p_inputs))
+
+	func calc(p_inputs):
+		var result = []
+		for i in range(neurons.size()):
+			result.append(neurons[i].calc(p_inputs))
+		return result
+
+class Neuron:
+	var weights = []
+	var bias
+	var inputs_size
+	func _init(p_inputs) -> void:
+		inputs_size = p_inputs
+	
+	func scramble():
+		for i in range(inputs_size):
+			weights.append(randf_range(-1, 1))
+		bias = randf_range(-1, 1)
+
+	func calc(p_inputs):
+		var sum = 0
+		for iter in range(p_inputs.size()):
+			sum += p_inputs[iter] * weights
+		sum += bias
+		sum = clamp(sum, -1, 1)
+		return sum
