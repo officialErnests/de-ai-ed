076e1e82a8eb03b181ad6e1921bec4294e099055
n0o0b090lv <ernestspursw@gmail.com>
2025-11-02 17:35:26 +0200

    Sample, demo

--- scripts/spiders_skeleton.gd
+++ scripts/spiders_skeleton.gd
@@ -3,13 +3,9 @@ extends PhysicalBoneSimulator3D
 
 # Called when the node enters the scene tree for the first time.
 
-var bones
-var base_leg_arr = []
-var base_legs_left_arr = []
-var base_legs_right_arr = []
-var upper_legs_arr = []
-var upper_legs_left_arr = []
-var upper_legs_right_arr = []
+var base_bone
+var leg_base_bones = []
+var leg_upper_bones = []
 
 var time = 0
 @export var debuger: Node3D
@@ -18,45 +14,70 @@ var main_bone_start_inverse
 
 func _ready() -> void:
 	physical_bones_start_simulation()
-	bones = get_children().filter(func(x): return x is PhysicalBone3D)
-	
+	var temp_bones = get_children().filter(func(x): return x is PhysicalBone3D)
+	for x in range(temp_bones.size() / 2.0):
+		if x == 0:
+			base_bone = Leg_bone.new(
+				temp_bones[x],
+				null,
+				null
+			)
+		leg_base_bones.append(Leg_bone.new(
+			temp_bones[x * 2 - 1],
+			base_bone.bone,
+			temp_bones[x * 2]
+		))
+		leg_upper_bones.append(Leg_bone.new(
+				temp_bones[x * 2],
+				temp_bones[x * 2 - 1],
+				null
+		))
+		leg_base_bones[x - 1].bone_next = leg_upper_bones[x - 1]
 
+var xx = 0
 func _physics_process(delta: float) -> void:
-	var a = 0
+	var a = -1
 	var b = 0
-	var s = 0
-	x += 1
-	if x % 10 == 0:
-		print(bones[1].transform.basis.x.dot(bones[0].transform.basis.x),
-		bones[1].transform.basis.y.dot(bones[0].transform.basis.y),
-		bones[1].transform.basis.z.dot(bones[0].transform.basis.z))
-	# debuger.basis = bones[1].transform.basis * (start_vector * (bones[0].transform.basis * bones[0].transform.basis.inverse()))
+	var s = 1
+	xx += 1
+	if xx % 10 == 0:
+		for iter_bone in leg_base_bones:
+			pass
+			print(iter_bone.getDirection())
 	
-	for base_leg in base_leg_arr:
-		base_leg.angular_velocity += base_leg.transform.basis.x * delta * 400 * a
+	for iter_bone in leg_base_bones:
+		iter_bone.addAngularVel(Vector3(
+			delta * 400 * a,
+			delta * 200 * b,
+			0
+		))
 
-	for base_leg in base_legs_left_arr:
-		base_leg.angular_velocity += base_leg.transform.basis.y * delta * 800 * b
+	for iter_bone in leg_upper_bones:
+		iter_bone.addAngularVel(Vector3(
+			delta * 400 * s,
+			0,
+			0
+		))
 
-	for base_leg in base_legs_right_arr:
-		base_leg.angular_velocity += base_leg.transform.basis.y * delta * 800 * -b
 
-	for upper_leg in upper_legs_arr:
-		upper_leg.angular_velocity += upper_leg.transform.basis.x * delta * 400 * s
-	# for upper_leg in upper_legs_arr:
-	# 	upper_leg.angular_velocity += upper_leg.transform.basis.get_euler(1) * delta * 20
-	# for base_leg in base_legs_left_arr:
-	# 	# passjoint_constraints/y/linear_limit_softness
-	# 	base_leg.linear_velocity += base_leg.transform.basis.x * delta * -1
-	pass
-	# for base_leg in base_legs_left_arr:
-	# 	base_leg.angular_velocity = base_leg.transform.basis.x * delta * -200
-
-class Leg:
-class Base_leg extends Bone:
-	func _init(p_bone: PhysicalBone3D, p_uper_bone):
+class Leg_bone extends Bone:
+	var bone_next
+	var bone_prev
+	func _init(p_bone: PhysicalBone3D, p_bone_next, p_bone_prev):
 		super (p_bone)
-		var
+		if p_bone_next: bone_next = p_bone_next
+		if p_bone_prev: bone_prev = p_bone_prev
+	
+	func getDirection():
+		return Vector3(bone.transform.basis.x.dot(bone_prev.transform.basis.x),
+				bone.transform.basis.y.dot(bone_prev.transform.basis.y),
+				bone.transform.basis.z.dot(bone_prev.transform.basis.z))
+	
+	func addAngularVel(p_velocity):
+		bone.angular_velocity += bone.transform.basis.x * p_velocity.x \
+								+ bone.transform.basis.y * p_velocity.y \
+								+ bone.transform.basis.z * p_velocity.z
+
 
 class Bone:
 	var bone
