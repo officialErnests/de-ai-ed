fb2156a6185bf46e803102b00a386f35ccead611
n0o0b090lv <ernestspursw@gmail.com>
2025-11-12 09:41:55 +0200

    v0.2.1 made graph

--- scripts/3d_graph_draw.gd
+++ scripts/3d_graph_draw.gd
@@ -15,6 +15,7 @@ extends Node
 @export_category("Misc")
 @export var text_packed_scene: PackedScene
 @export var value_parent: Node3D
+@export var graph_material: BaseMaterial3D
 
 
 var graph: Node3D
@@ -41,7 +42,15 @@ func updateGraph() -> void:
 
 		var graph_value_mesh := MeshInstance3D.new()
 		graph_value_mesh.mesh = BoxMesh.new()
+		graph_value_mesh.material_override = graph_material
 		graph_value_mesh.mesh.size = Vector3(segment_size.x, iter_item_value, z_size)
 		graph_value_mesh.position.x = x_axis_offset * segment_size.x + x_axis_offset * x_padding + 0.1 - segment_size.x / 2
 		graph_value_mesh.position.y = iter_item_value / 2 + 0.1
 		value_parent.add_child(graph_value_mesh)
+
+		var graph_value_text := text_packed_scene.instantiate()
+		graph_value_text.value = str(iter_item_value)
+		graph_value_text.update()
+		graph_value_text.position.x = graph_value_mesh.position.x - segment_size.x / 2
+		graph_value_text.position.y = iter_item_value + 0.1 + 0.2
+		value_parent.add_child(graph_value_text)
